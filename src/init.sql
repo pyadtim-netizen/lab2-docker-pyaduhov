8bcb399e661971efe64720b2dab92d7d4ef30843
Pyaduhov <pyadtim@gmail.com>
2026-04-08 11:10:52 +0300

    doing-init-docker

--- src/init.sql
+++ src/init.sql
@@ -1,20 +1,24 @@
-CREATE DATABASE IF NOT EXISTS users_db 
-DEFAULT CHARACTER SET utf8mb4 
+-- Создаём базу данных с поддержкой кириллицы
+CREATE DATABASE IF NOT EXISTS users_db
+DEFAULT CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
 USE users_db;
 
+-- Таблица для хранения пользователей
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    full_name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE
-);
+    login VARCHAR(50) UNIQUE NOT NULL,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-INSERT INTO users (full_name, email) VALUES 
-('Тестовый Пользователь', 'test@example.com'),
-('Алексей Жердев', 'alexey.zherdev@misis.ru'),
-('Пядухов Тимофей Андреевич', 'pyaduhov@university.ru');
+-- Тестовые данные
+INSERT INTO users(login, first_name, last_name) VALUES
+('admin', 'Иван', 'Иванов'),
+('user1', 'Петя', 'Петров');
 
-CREATE USER IF NOT EXISTS 'pyaduhov.tim'@'%' IDENTIFIED BY 'lab_pass123';
-GRANT ALL PRIVILEGES ON users_db.* TO 'pyaduhov.tim'@'%';
+-- Создаём пользователя и даём права
+CREATE USER IF NOT EXISTS 'my_app'@'%' IDENTIFIED BY 'my_password';
+GRANT ALL PRIVILEGES ON users_db.* TO 'my_app'@'%';
 FLUSH PRIVILEGES;
